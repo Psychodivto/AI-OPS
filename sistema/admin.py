8aca04898170fc64ed1fafbc990f23498b2fced1
psychodivto <condedieo48@gmail.com>
2024-02-05 00:00:01 -0600

    Refactor variable names for clarity

--- sistema/admin.py
+++ sistema/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Auto, Propietario, CustomUser
+
+admin.site.register(Auto)
+admin.site.register(Propietario)
+admin.site.register(CustomUser)
+
+
