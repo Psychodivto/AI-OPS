4c9f31e226a0d95dcddec46b354fe4b72dcb0be6
psychodivto <condedieo48@gmail.com>
2024-02-05 00:15:10 -0600

    Delete unused files and pycache files

--- autos/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Auto, Propietario
-# Register your models here.
-
-admin.site.register(Auto)
-admin.site.register(Propietario) 
--- autos/views.py
+++
@@ -1,7 +0,0 @@
-from django.http import JsonResponse
-from graphene_django.views import GraphQLView
-
-# Create your views here.
-
-def ping(request):
-    return JsonResponse({'status': 'ok'})
