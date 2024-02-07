f318185622cc1f05d35cc8923b04d5d171a06595
psychodivto <condedieo48@gmail.com>
2024-02-06 20:33:23 -0600

    Update server bind address to use dynamic port

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,7 +10,7 @@ python manage.py wait_for_db --settings=vehiculares.settings.production
 python manage.py migrate --settings=vehiculares.settings.production
 
 echo 'Running server...'
-gunicorn --env DJANGO_SETTINGS_MODULE=vehiculares.settings.production vehiculares.wsgi:application --bind 0.0.0.0:8000
+gunicorn --env DJANGO_SETTINGS_MODULE=vehiculares.settings.production vehiculares.wsgi:application --bind 0.0.0.0:$PORT
 
 
 
