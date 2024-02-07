7b8e718d90aab3e3b024ab84860ad5a99ed9fd76
psychodivto <condedieo48@gmail.com>
2024-02-06 19:55:09 -0600

    Remove unused files and configurations

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,7 +11,6 @@ python manage.py migrate --settings=vehiculares.settings.production
 
 echo 'Running server...'
 gunicorn --env DJANGO_SETTINGS_MODULE=vehiculares.settings.production vehiculares.wsgi:application --bind 0.0.0.0:8000
-```
 
 
 
